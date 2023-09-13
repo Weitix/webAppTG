fccf7a90023a250e1f66590941595467c8430da5
Kirill Gubin <84503067+Weitix@users.noreply.github.com>
2023-09-13 20:18:25 +0300

    Create payment.js

---
+++ payment.js
@@ -0,0 +1,12 @@
+// Обработчики для кнопок выбора способа оплаты
+document.getElementById('idar-button').addEventListener('click', function() {
+    window.location.href = 'index2.html'; // Переход на страницу index2.html
+});
+
+document.getElementById('easywallet-button').addEventListener('click', function() {
+    window.location.href = 'index2.html'; // Переход на страницу index2.html
+});
+
+document.getElementById('telcellWallet-button').addEventListener('click', function() {
+    window.location.href = 'index2.html'; // Переход на страницу index2.html
+});
