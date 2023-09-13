5b05b505accce0c31e86cc203373741a3abc713e
Kirill Gubin <84503067+Weitix@users.noreply.github.com>
2023-09-13 19:57:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,28 @@
      
       //Telegram.WebApp.sendData(data);
     //});
+document.getElementById('btc-button').addEventListener('click', function() {
+    alert('Выбрана криптовалюта BTC');
+});
 
+document.getElementById('ltc-button').addEventListener('click', function() {
+    alert('Выбрана криптовалюта LTC');
+});
+
+document.getElementById('dash-button').addEventListener('click', function() {
+    alert('Выбрана криптовалюта DASH');
+});
+
+document.getElementById('usdt-button').addEventListener('click', function() {
+    alert('Выбрана криптовалюта USDT');
+});
+
+// Обработчик для отправки формы
+document.getElementById('crypto-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Предотвращаем стандартное поведение формы (перезагрузку страницы)
+    
+    // Получаем значения из полей формы
+    const walletAddress = document.getElementById
 
 let params = new URLSearchParams(window.location.search);
 let userId = params.get('userid');
